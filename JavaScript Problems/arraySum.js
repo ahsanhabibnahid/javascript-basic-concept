9aa8a75b210687a86693c7fb9acc16de36fde516
ahsanhabibnahid <ahsanhabib3239@gmail.com>
2021-08-03 23:16:13 +0600

    array sum

---
+++ JavaScript Problems/arraySum.js
@@ -0,0 +1,15 @@
+
+function arraySum(numbers) {
+    var sum = 0
+
+    for (i = 0; i < numbers.length; i++) {
+        var sum = numbers[i] + sum;
+    }
+    return sum;
+}
+
+var numbers = [10,20,20]
+
+var result = arraySum(numbers)
+
+console.log(result)
---
+++ JavaScript Problems/maxNumber.js
@@ -0,0 +1,13 @@
+var a = 50;
+var b = 40;
+var c = 30;
+
+if(a>b && a>c){
+    console.log('a is greater than b and c')
+}
+else if(b>a && b>c){
+    console.log('b is greater than a and c')
+}
+else{
+    console.log('c is greater than a and b')
+}
