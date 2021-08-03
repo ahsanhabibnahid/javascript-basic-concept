567f6bd98cd73eba2c324ca7ef7309d57720dd56
ahsanhabibnahid <ahsanhabib3239@gmail.com>
2021-08-03 16:40:55 +0600

    random

---
+++ JavaScript Problems/random.js
@@ -0,0 +1,11 @@
+// var number = 53.49;
+// var result1 = Math.floor(number);
+// var result2 = Math.ceil(number)
+// var result3 = Math.round(number)
+// console.log(result3)
+
+//
+
+var randomNumber = Math.random()* 1000;
+var dice = Math.round(randomNumber);
+console.log(dice)
